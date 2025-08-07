ade59aa791042ed8e3c3b7f156be030a420278ec
dlitun <dlitun@ya.ru>
2025-08-07 16:47:03 +0400

    KS-5-8: create conditionals

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
 package org.example
 
-fun main() {
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 
+fun main() {
+    val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
+    val quotes: Elements = doc.select("div.sc-2aegk7-2")
+
+    for (quote: Element in quotes) {
+        println("${quote.text()}")
+    }
 }
